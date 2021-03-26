bcf9b06a81be744529c2e0e5dcf2a4fb7ddc819b
Mhotsa <78471518+Mhotsa@users.noreply.github.com>
2021-03-26 17:19:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("My First Console Project");
+
+var first_name = "Mhotsav";
+var last_name = "Gandotra";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var number_1 = 4;
+var number_2 = 7;
+var add = number_1 + number_2;
+var sub = number_2 - number_1;
+var mul = number_1 * number_2;
+var min = Math.min(number_1, number_2);
+var max = Math.max(number_1, number_2);
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(min);
+console.log(max);
